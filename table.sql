73277014bd4c4d6472e5b7d505701f43034996d1
ollehing <ollehing@gmail.com>
2019-04-30 18:45:25 +0900

    front 작업 및 서비스 작업중.

--- table.sql
+++ table.sql
@@ -22,11 +22,24 @@ CREATE TABLE `smtp_reservation` (
   `email_title` varchar(50) DEFAULT NULL,
   `email_form` text,
   `customer_address` text,
-  `reservation_date` datetime DEFAULT NULL,
   `sys_send_date` datetime DEFAULT NULL,
   `state` char(2) DEFAULT 'N',
   `sysdate` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`reservation_code`),
-  UNIQUE KEY `smtp_reservation_reservation_code_uindex` (`reservation_code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+  UNIQUE KEY `smtp_reservation_reservation_code_uindex` (`reservation_code`),
+  KEY `smtp_reservation_smtp_reservation_group_group_code_fk` (`group_code`),
+  CONSTRAINT `smtp_reservation_smtp_reservation_group_group_code_fk` FOREIGN KEY (`group_code`) REFERENCES `smtp_reservation_group` (`group_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
+
+
+CREATE TABLE `smtp_reservation_group` (
+  `group_code` char(15) NOT NULL,
+  `group_title` varchar(50) DEFAULT NULL,
+  `reservation_date` datetime DEFAULT NULL,
+  `sys_send_date` datetime DEFAULT NULL,
+  `state` char(2) DEFAULT 'N',
+  `sysdate` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`group_code`),
+  UNIQUE KEY `smtp_reservation_group_group_code_uindex` (`group_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci
 
